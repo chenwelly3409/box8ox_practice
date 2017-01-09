79ff02a1c7a514023bd3421e4108ebde6a7f11c4
chenwelly3409 <chenwelly3409@gmail.com>
2017-01-09 22:00:53 +0800

    Card

--- R/Dcard爬蟲/Dcard.R
+++ R/Dcard爬蟲/Dcard.R
@@ -1,11 +1,11 @@
 library(jsonlite)
 
 Crawler <- function() {
-	url = "https://www.dcard.tw/_api/forums/whysoserious/posts?popular=false&before="
+	url = "https://www.dcard.tw/_api/forums/whysoserious/posts?popular=false&before=" #文章看板
 
 	Dcard <<-fromJSON("https://www.dcard.tw/_api/forums/whysoserious/posts?popular=false")
 
-	for( i in c( 1 : 1500 ) ) {
+	for( i in c( 1 : 3 ) ) {  #爬幾頁
 		id = Dcard[30,1];
 		temp_url = paste( url, id, sep = "" );
 		Dcard <<- rbind( Dcard, fromJSON(temp_url) );
@@ -20,4 +20,4 @@ Dcard = as.matrix(Dcard)
 
 write.table(Dcard, file = "Dcard.CSV", sep = "," )
 
-
+#有問題歡迎來問
